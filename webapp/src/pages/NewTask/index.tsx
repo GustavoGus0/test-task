08d0d94235756bc97f43bc1c2eafff2b97f88b7e
GustavoGus0 <manunknown964@gmail.com>
2025-04-25 17:59:02 +0500

    Added functionality to add executor id to parameters to pass on backend. Now the task should appear in task list at executor

--- webapp/src/pages/NewTask/index.tsx
+++ webapp/src/pages/NewTask/index.tsx
@@ -1,14 +1,19 @@
 import { zNewTaskTrpcInput } from '@management/backend/src/router/newTask/input'
 import { TaskPriority, TaskStatus } from '@management/backend/src/utils/types'
+import cn from 'classnames'
 import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 
+import { icons } from '../../assets/icons'
 import { Form } from '../../components/Form'
+import { Loader } from '../../components/Loader'
 import { Segment } from '../../components/Segment'
 import { useAlerts, useMe } from '../../lib/ctx'
 import { trpc } from '../../lib/trpc'
 import { getCyrillicPriority } from '../../utils/getCyrillic'
+
+import css from './index.module.scss'
 
 export interface IInitialValues {
   title: string
@@ -32,13 +37,14 @@ export const NewTask = () => {
   const defaultPriority: IInitialValues['priority'] = null
   const defaultStatus: TaskStatus = 'to-do'
   const createTask = trpc.newTask.useMutation()
+  const [taskExecutorId, setTaskExecutorId] = useState<string | null>(null)
 
   const initialValues: IInitialValues = {
     title: '',
     description: '',
     priority: defaultPriority,
     status: defaultStatus,
-    assignedToId: null,
+    assignedToId: taskExecutorId,
   }
 
   const formik = useFormik({
@@ -56,26 +62,83 @@ export const NewTask = () => {
   })
   return (
     <Segment title={'Новая задача'}>
-      <Form
-        key={formKey}
-        formik={formik}
-        legend={'Создайте новую задачу'}
-        inputs={[
-          {
-            name: 'title',
-            label: 'Заголовок',
-          },
-          { name: 'description', label: 'Описание', type: 'textarea' },
-        ]}
-        selectorInputs={[
-          {
-            translatorFunction: getCyrillicPriority as (priority: string) => string,
-            name: 'priority',
-            label: 'Приоритет',
-            parameters: ['low', 'medium', 'high'],
-          },
-        ]}
-      />
+      <div className={css.formWrapper}>
+        <Form
+          key={formKey}
+          formik={formik}
+          legend={'Создайте новую задачу'}
+          inputs={[
+            {
+              name: 'title',
+              label: 'Заголовок',
+            },
+            { name: 'description', label: 'Описание', type: 'textarea' },
+          ]}
+          selectorInputs={[
+            {
+              translatorFunction: getCyrillicPriority as (priority: string) => string,
+              name: 'priority',
+              label: 'Приоритет',
+              parameters: ['low', 'medium', 'high'],
+            },
+          ]}
+        />
+        {me.role === 'MANAGER' && (
+          <ExecutorsSelector<string | null>
+            taskExecutorId={taskExecutorId}
+            setTaskExecutorId={setTaskExecutorId}
+          />
+        )}
+      </div>
     </Segment>
+  )
+}
+
+const ExecutorsSelector = <T,>({
+  taskExecutorId,
+  setTaskExecutorId,
+}: {
+  taskExecutorId: T
+  setTaskExecutorId: Dispatch<SetStateAction<T>>
+}) => {
+  const [showExecutors, setShowExecutors] = useState<boolean>(false)
+  const { data, error, isLoading, isFetching } = trpc.getExecutors.useQuery()
+
+  const handleSetExecutor = (parameter: T) => {
+    if (taskExecutorId === parameter) {
+      return setTaskExecutorId(null as T)
+    }
+    setTaskExecutorId(parameter)
+  }
+  if (isLoading || isFetching) {
+    return <Loader />
+  }
+  if (!data || error || data?.length === 0) {
+    return null
+  }
+
+  return (
+    <div className={css.executorsSelector}>
+      <button onClick={() => setShowExecutors((prev) => !prev)} className={css.selectorButton}>
+        <div className={css.icon}>{icons.newTask()}</div>
+        <span>Назначить на подчинённого</span>
+      </button>
+      <ul className={css.executorsList}>
+        {showExecutors &&
+          data.map((executor) => (
+            <li key={executor.id} className={css.executorItem}>
+              <button
+                onClick={() => handleSetExecutor(executor.id as T)}
+                className={cn({
+                  [css.executorButton]: true,
+                  [css.active]: taskExecutorId === executor.id,
+                })}
+              >
+                {executor.lastName} {executor.firstName} {executor.patronymic}
+              </button>
+            </li>
+          ))}
+      </ul>
+    </div>
   )
 }
