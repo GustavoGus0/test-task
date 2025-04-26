ed882952de4b31aa0a227758055278ea403a10d3
GustavoGus0 <manunknown964@gmail.com>
2025-04-26 05:23:51 +0500

    Assigned by is displayed on main page with all tasks. Remade the construction of task

--- webapp/src/components/Task/index.tsx
+++ webapp/src/components/Task/index.tsx
@@ -2,16 +2,13 @@ import { TaskPriority, TaskStatus } from '@management/backend/src/utils/types'
 import cn from 'classnames'
 import { Link } from 'react-router'
 
-import { icons } from '../../assets/icons'
 import { useMe } from '../../lib/ctx'
 import { getViewTaskRoute } from '../../lib/routes'
-import { getCyrillicStatus } from '../../utils/getCyrillic'
+import { getCyrillicPriority, getCyrillicStatus } from '../../utils/getCyrillic'
 
 import css from './index.module.scss'
 
 export interface ITask {
-  completed?: boolean
-  cancelled?: boolean
   id: string
   title: string
   description: string
@@ -22,8 +19,6 @@ export interface ITask {
 }
 
 export const Task = ({
-  completed = false,
-  cancelled = false,
   id,
   title,
   description,
@@ -37,49 +32,37 @@ export const Task = ({
     <Link to={getViewTaskRoute({ taskId: id })}>
       <li className={css.task}>
         <div className={css.titleBox}>
-          <div className={css.priorityAndTitle}>
-            <div
-              className={
-                completed
-                  ? cn({
-                      [css.priority]: true,
-                      [css.completed]: true,
-                    })
-                  : cancelled
-                    ? cn({
-                        [css.priority]: true,
-                        [css.cancelled]: true,
-                      })
-                    : cn({
-                        [css.priority]: true,
-                        [css.high]: priority === 'high',
-                        [css.medium]: priority === 'medium',
-                        [css.low]: priority === 'low',
-                      })
-              }
-            >
-              {completed && icons.checkMark()}
-              {cancelled && icons.cross()}
-            </div>
-            <h3 className={css.title}>{title}</h3>
+          <h3 className={css.title}>{title}</h3>
+          <p className={css.description}>{description}</p>
+        </div>
+        <div className={css.info}>
+          <div className={cn({ [css.status]: true, [css.bar]: true })}>
+            {getCyrillicStatus(status)}
           </div>
-          <div className={css.statusAndAssignedBy}>
-            <p className={css.status}>{getCyrillicStatus(status)}</p>
-            <p className={css.status}>
-              {me?.role === 'EXECUTOR' && (
-                <span>{createdById === me?.id ? 'Моя задача' : 'Назначена руководителем'}</span>
-              )}
-              {me?.role === 'MANAGER' && (
-                <span>
-                  {createdById === me?.id && assignedToId === me?.id
-                    ? 'Моя задача'
-                    : 'Назначена исполнителю'}
-                </span>
-              )}
-            </p>
+          <div
+            className={cn({
+              [css.priority]: true,
+              [css.bar]: true,
+              [css.red]: priority === 'high',
+              [css.yellow]: priority === 'medium',
+              [css.green]: priority === 'low',
+            })}
+          >
+            {getCyrillicPriority(priority)} приоритет
+          </div>
+          <div className={cn({ [css.assignedBy]: true, [css.bar]: true })}>
+            {me?.role === 'EXECUTOR' && (
+              <span>{createdById === me?.id ? 'Моя задача' : 'Назначена руководителем'}</span>
+            )}
+            {me?.role === 'MANAGER' && (
+              <span>
+                {createdById === me?.id && assignedToId !== me?.id
+                  ? 'Назначена исполнителю'
+                  : 'Моя задача'}
+              </span>
+            )}
           </div>
         </div>
-        <p className={css.description}>{description}</p>
       </li>
     </Link>
   )
