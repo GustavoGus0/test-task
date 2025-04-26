9750d30fea7ef60fde7fe6a4295f1e72b25d04db
GustavoGus0 <manunknown964@gmail.com>
2025-04-26 19:23:25 +0500

    manager also must have filtering by time

--- backend/src/router/getTasks/index.ts
+++ backend/src/router/getTasks/index.ts
@@ -62,7 +62,17 @@ export const getTasksTrpcRoute = trpc.procedure
       })
       return { tasks }
     } else if (input && ctx.me.role === 'MANAGER') {
-      const { byTasks, byDate, byPriority, byStatus } = input
+      const { byTasks, byDate, byPriority, byStatus, byTime } = input
+      let dateFilter = {}
+      if (byTime === 'on-today') {
+        const oneDayAgo = new Date()
+        oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+        dateFilter = { gte: oneDayAgo }
+      } else if (byTime === 'on-week') {
+        const oneWeekAgo = new Date()
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+        dateFilter = { gte: oneWeekAgo }
+      }
       const tasks = await ctx.prisma.task.findMany({
         where: {
           createdById: ctx.me.id,
@@ -74,6 +84,7 @@ export const getTasksTrpcRoute = trpc.procedure
                 : ctx.me.id,
           priority: byPriority || undefined,
           status: byStatus ? byStatus : { notIn: ['completed', 'cancelled'] },
+          createdAt: dateFilter,
         },
         select: {
           id: true,
