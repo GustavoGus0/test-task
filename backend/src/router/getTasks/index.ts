7994fb3691b829051340669b5d3df013e492bcaa
GustavoGus0 <manunknown964@gmail.com>
2025-05-02 16:19:51 +0500

    Made getTasks route reusable logic for cases on every role

--- backend/src/router/getTasks/index.ts
+++ backend/src/router/getTasks/index.ts
@@ -8,149 +8,64 @@ export const getTasksTrpcRoute = trpc.procedure
     if (!ctx.me) {
       throw new Error('UNAUTHORIZED')
     }
+    const { byTasks, byDate, byPriority, byStatus, byTime } = input ?? {}
 
-    if (input && ctx.me.role === 'EXECUTOR' && ctx.me.managerId === null) {
-      const { byTasks, byDate, byPriority, byStatus, byTime } = input
-
-      let dateFilter = {}
+    const defineDateFilter = () => {
       if (byTime === 'on-today') {
         const oneDayAgo = new Date()
         oneDayAgo.setDate(oneDayAgo.getDate() - 1)
-        dateFilter = { gte: oneDayAgo }
+        return { gte: oneDayAgo }
       } else if (byTime === 'on-week') {
         const oneWeekAgo = new Date()
         oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
-        dateFilter = { gte: oneWeekAgo }
+        return { gte: oneWeekAgo }
       }
-
-      const tasks = await ctx.prisma.task.findMany({
-        where: {
-          createdById: ctx.me.id,
-          assignedToId: byTasks === 'executors' ? { not: ctx.me.id } : ctx.me.id,
-          priority: byPriority || undefined,
-          status: byStatus ? byStatus : { notIn: ['completed', 'cancelled'] },
-          createdAt: dateFilter,
-        },
-        select: {
-          id: true,
-          title: true,
-          description: true,
-          status: true,
-          priority: true,
-          createdById: true,
-          createdBy: {
-            select: {
-              id: true,
-              firstName: true,
-              lastName: true,
-              patronymic: true,
-            },
-          },
-          assignedTo: {
-            select: {
-              id: true,
-              firstName: true,
-              lastName: true,
-              patronymic: true,
-            },
-          },
-        },
-        orderBy: {
-          createdAt: byDate === 'new' ? 'desc' : 'asc',
-        },
-      })
-      return { tasks }
-    } else if (input && ctx.me.role === 'EXECUTOR' && ctx.me.managerId !== null) {
-      const { byTasks, byDate, byPriority, byStatus, byTime } = input
-
-      let dateFilter = {}
-      if (byTime === 'on-today') {
-        const oneDayAgo = new Date()
-        oneDayAgo.setDate(oneDayAgo.getDate() - 1)
-        dateFilter = { gte: oneDayAgo }
-      } else if (byTime === 'on-week') {
-        const oneWeekAgo = new Date()
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
-        dateFilter = { gte: oneWeekAgo }
-      }
-
-      const tasks = await ctx.prisma.task.findMany({
-        where: {
-          createdById:
-            byTasks === 'all' ? undefined : byTasks === 'managers' ? ctx.me.managerId : ctx.me.id,
-          assignedToId: byTasks === 'executors' ? { not: ctx.me.id } : ctx.me.id,
-          priority: byPriority || undefined,
-          status: byStatus ? byStatus : { notIn: ['completed', 'cancelled'] },
-          createdAt: dateFilter,
-        },
-        select: {
-          id: true,
-          title: true,
-          description: true,
-          status: true,
-          priority: true,
-          createdById: true,
-          createdBy: {
-            select: {
-              id: true,
-              firstName: true,
-              lastName: true,
-              patronymic: true,
-            },
-          },
-          assignedTo: {
-            select: {
-              id: true,
-              firstName: true,
-              lastName: true,
-              patronymic: true,
-            },
-          },
-        },
-        orderBy: {
-          createdAt: byDate === 'new' ? 'desc' : 'asc',
-        },
-      })
-      return { tasks }
-    } else if (input && ctx.me.role === 'MANAGER') {
-      const { byTasks, byDate, byPriority, byStatus, byTime } = input
-      let dateFilter = {}
-      if (byTime === 'on-today') {
-        const oneDayAgo = new Date()
-        oneDayAgo.setDate(oneDayAgo.getDate() - 1)
-        dateFilter = { gte: oneDayAgo }
-      } else if (byTime === 'on-week') {
-        const oneWeekAgo = new Date()
-        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
-        dateFilter = { gte: oneWeekAgo }
-      }
-      const tasks = await ctx.prisma.task.findMany({
-        where: {
-          createdById: ctx.me.id,
-          assignedToId:
-            byTasks === 'all'
-              ? undefined
-              : byTasks === 'executors'
-                ? { not: ctx.me.id }
-                : ctx.me.id,
-          priority: byPriority || undefined,
-          status: byStatus ? byStatus : { notIn: ['completed', 'cancelled'] },
-          createdAt: dateFilter,
-        },
-        select: {
-          id: true,
-          title: true,
-          description: true,
-          status: true,
-          priority: true,
-          createdById: true,
-          assignedToId: true,
-        },
-        orderBy: {
-          createdAt: byDate === 'new' ? 'desc' : 'asc',
-        },
-      })
-      return { tasks }
     }
-    return null
+
+    const defineAssignedToIdFilter = () => {
+      if (ctx.me?.role === 'MANAGER') {
+        return byTasks === 'my'
+          ? ctx.me.id
+          : byTasks === 'executors'
+            ? { not: ctx.me.id }
+            : undefined
+      } else if (ctx.me?.role === 'EXECUTOR' && ctx.me?.managerId !== null) {
+        return ctx.me.id
+      } else {
+        return ctx.me?.id
+      }
+    }
+
+    const defineCreatedByIdFilter = () => {
+      if (ctx.me?.role === 'MANAGER') {
+        return ctx.me.id
+      } else if (ctx.me?.role === 'EXECUTOR' && ctx.me?.managerId !== null) {
+        return byTasks === 'my' ? ctx.me.id : byTasks === 'managers' ? ctx.me.managerId : undefined
+      } else {
+        return ctx.me?.id
+      }
+    }
+
+    const tasks = await ctx.prisma.task.findMany({
+      where: {
+        createdById: defineCreatedByIdFilter(),
+        assignedToId: defineAssignedToIdFilter(),
+        priority: byPriority || undefined,
+        status: byStatus ? byStatus : { notIn: ['completed', 'cancelled'] },
+        createdAt: defineDateFilter(),
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        status: true,
+        priority: true,
+        createdById: true,
+        assignedToId: true,
+      },
+      orderBy: {
+        createdAt: byDate === 'new' ? 'desc' : 'asc',
+      },
+    })
+    return { tasks }
   })
